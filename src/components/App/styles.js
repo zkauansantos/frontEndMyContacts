1eb1f4b96f2ab7dde25368d050c54e5c055311e5
zkauansantos <kauansantosdepontes@gmail.com>
2022-12-08 14:25:11 -0300

    "header" contacts list criated

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -4,5 +4,5 @@ export const Container = styled.div`
     width: 100%;
     max-width: 500px;
     margin: 0 auto;
-    
+
 `;
---
+++ src/components/ContactsList/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 32px;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+        color: #222;
+        font-size: 24px;
+    }
+
+    a {
+        color: ${({ theme }) => theme.colors.primary.main};
+        text-decoration: none;
+        padding: 10px 16px;
+        border-radius: 5px;
+        font-weight: bold;
+        border: 2px solid ${({ theme }) => theme.colors.primary.main};
+        transition: 0.3s ease-in;
+
+        &:hover{
+            background:  ${({ theme }) => theme.colors.primary.main};
+            color: #fff;
+        }
+    }
+`;
