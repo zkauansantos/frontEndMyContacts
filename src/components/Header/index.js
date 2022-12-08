1fb58ec0675291c2fa1bf412cc3ab8a218a0dd98
zkauansantos <kauansantosdepontes@gmail.com>
2022-12-08 13:30:08 -0300

    header criated

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import { Container, InputSearchContainer } from './styles';
+
+import logo from '../../assets/imgs/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="MyContacts" width="201" />
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar Contato" />
+      </InputSearchContainer>
+    </Container>
+  );
+}
