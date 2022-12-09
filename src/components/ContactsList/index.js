b5630ed6428e40637e3598b8279322cf1208ae6e
zkauansantos <kauansantosdepontes@gmail.com>
2022-12-09 11:08:16 -0300

    criating contacts list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+ Container, Header, ListContainer, Card,
+} from './styles';
+import arrow from '../../assets/imgs/icons/arrow.svg';
+import edit from '../../assets/imgs/icons/edit.svg';
+import trash from '../../assets/imgs/icons/trash.svg';
 
 export default function ContactsList() {
     return (
@@ -7,6 +12,36 @@ export default function ContactsList() {
           <strong> 3 contatos </strong>
           <a href="/"> Novo Contato </a>
         </Header>
+
+        <ListContainer>
+          <header>
+            <button type="button">
+              <span>Nome</span>
+              <img src={arrow} alt="arrow" />
+            </button>
+          </header>
+
+          <Card>
+            <div className="info">
+              <div className="contact-name">
+                <strong> Kauan Santos </strong>
+                <small> instagram </small>
+              </div>
+              <span>kauan@devaca.com.br</span>
+              <span>(61) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <a href="/">
+                <img src={edit} alt="edit" />
+              </a>
+
+              <button type="button">
+                <img src={trash} alt="delete" />
+              </button>
+            </div>
+          </Card>
+        </ListContainer>
       </Container>
     );
 }
--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -1,31 +1,84 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    margin-top: 32px;
+  margin-top: 32px;
 `;
 
 export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  strong {
+    color: #222;
+    font-size: 24px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    padding: 10px 16px;
+    border-radius: 5px;
+    font-weight: bold;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    transition: 0.3s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+    }
+
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+  }
+`;
+
+export const Card = styled.div`
+    background: #fff;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    padding: 16px;
+    border-radius: 4px;
     display: flex;
     align-items: center;
     justify-content: space-between;
 
-    strong {
-        color: #222;
-        font-size: 24px;
-    }
+    .info {
+        .contact-name {
+            display: flex;
+            align-items: center;
 
-    a {
-        color: ${({ theme }) => theme.colors.primary.main};
-        text-decoration: none;
-        padding: 10px 16px;
-        border-radius: 5px;
-        font-weight: bold;
-        border: 2px solid ${({ theme }) => theme.colors.primary.main};
-        transition: 0.3s ease-in;
+            small {
+              background: ${({ theme }) => theme.colors.primary.lighter};
+              color: ${({ theme }) => theme.colors.primary.main};
+              font-weight: bold;
+              padding: 4px;
+              text-transform: uppercase;
+              border-radius: 4px;
+              margin-left: 8px;
+            }
+        }
 
-        &:hover{
-            background:  ${({ theme }) => theme.colors.primary.main};
-            color: #fff;
+        span {
+            font-size: 14px;
+            display: block;
         }
     }
 `;
