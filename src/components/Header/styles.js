0cdcd550170ab8e47862c4c929eb6bd5d44786c8
zkauansantos <kauansantosdepontes@gmail.com>
2022-12-13 14:24:31 -0300

    refactored code

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -6,24 +6,5 @@ export const Container = styled.header`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-`;
-
-export const InputSearchContainer = styled.div`
-    margin-top: 48px;
-    width: 100%;
-
-    input {
-        width: 100%;
-        border-radius: 25px;
-        height: 50px;
-        background: #fff;
-        border: none;
-        box-shadow: 8px 4px 10px rgba(0, 0, 0, 0.04);
-        outline: 0;
-        padding: 0 16px;
-
-        &::placeholder {
-          color: ${({ theme }) => theme.colors.gray[200]};
-        }
-    }
+    margin-bottom: 48px;
 `;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,90 @@
-import ContactsList from '../../components/ContactsList';
+import { Link } from 'react-router-dom';
+import {
+ Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+import arrow from '../../assets/imgs/icons/arrow.svg';
+import edit from '../../assets/imgs/icons/edit.svg';
+import trash from '../../assets/imgs/icons/trash.svg';
 
 export default function Home() {
     return (
-      <ContactsList />
+      <Container>
+
+        <InputSearchContainer>
+          <input type="text" placeholder="Pesquisar Contato" />
+        </InputSearchContainer>
+
+        <Header>
+          <strong> 3 contatos </strong>
+          <Link to="/new"> Novo Contato </Link>
+        </Header>
+
+        <ListContainer>
+          <header>
+            <button type="button">
+              <span>Nome</span>
+              <img src={arrow} alt="arrow" />
+            </button>
+          </header>
+
+          <Card>
+            <div className="info">
+              <div className="contact-name">
+                <strong> Kauan Santos </strong>
+                <small> instagram </small>
+              </div>
+              <span>kauan@devaca.com.br</span>
+              <span>(61) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <Link to="/edit/123">
+                <img style={{ marginTop: 3 }} src={edit} alt="edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="delete" />
+              </button>
+            </div>
+          </Card>
+          <Card>
+            <div className="info">
+              <div className="contact-name">
+                <strong> Kauan Santos </strong>
+                <small> instagram </small>
+              </div>
+              <span>kauan@devaca.com.br</span>
+              <span>(61) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <Link to="/edit/123">
+                <img style={{ marginTop: 3 }} src={edit} alt="edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="delete" />
+              </button>
+            </div>
+          </Card>
+          <Card>
+            <div className="info">
+              <div className="contact-name">
+                <strong> Kauan Santos </strong>
+                <small> instagram </small>
+              </div>
+              <span>kauan@devaca.com.br</span>
+              <span>(61) 99999-9999</span>
+            </div>
+
+            <div className="actions">
+              <Link to="/edit/123">
+                <img style={{ marginTop: 3 }} src={edit} alt="edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="delete" />
+              </button>
+            </div>
+          </Card>
+        </ListContainer>
+      </Container>
     );
 }
