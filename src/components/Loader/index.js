1cd16d99486ecc5c7bf4687e89983c181fc978f9
zkauansantos <kauansantosdepontes@gmail.com>
2022-12-19 14:27:33 -0300

    criated Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,3 +1,11 @@
-export default function Loader () {
-  return 'Loader'
+import ReactDOM from 'react-dom';
+import { Overlay } from './styles';
+
+export default function Loader() {
+  return ReactDOM.createPortal(
+    <Overlay>
+      <div className="loader" />
+    </Overlay>,
+   document.getElementById('loader-root'),
+);
 }
