86791824a480cacd66834d7f18811f478895d0a9
zkauansantos <kauansantosdepontes@gmail.com>
2022-12-21 11:48:32 -0300

    abstracted logic and vusiness rule in custom hook

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -6,34 +6,28 @@ import Button from '../Button';
 import FormGroup from '../FormGroup';
 import { Form, ButtonContainer } from './styles';
 import isEmailValid from '../../utils/isEmailValid';
+import useErrors from '../../hooks/useErrors';
 
 export default function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
 
   function handleNameChange(event) {
     setName(event.target.value);
 
     if (!event.target.value) {
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'name', message: 'Nome é obrigatório' },
-      ]);
+      setError({ field: 'name', message: 'O nome é obrigatório' });
     } else {
-      setErrors((prevState) => prevState.filter(
-        (error) => error.field !== 'name',
-      ));
+      removeError('name');
       }
     }
+
   function handleNameBlur(event) {
     if (!event.target.value) {
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'name', message: 'Nome é obrigatório' },
-      ]);
+      setError({ field: 'name', message: 'O nome é obrigatório' });
     }
   }
 
@@ -41,23 +35,10 @@ export default function ContactForm({ buttonLabel }) {
     setEmail(event.target.value);
 
     if (event.target.value && !isEmailValid(event.target.value)) {
-      const errorAlreadyExists = errors.find((error) => error.field === 'email');
-
-      if (errorAlreadyExists) return;
-
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'email', message: 'E-mail Inválido' },
-      ]);
+      setError({ field: 'email', message: 'Este e-mail não é válido' });
     } else {
-      setErrors((prevState) => prevState.filter(
-        (error) => error.field !== 'email',
-      ));
+      removeError('email');
     }
-  }
-
-  function getErrorMessageByFieldName(fieldName) {
-    return errors.find((error) => error.field === fieldName)?.message;
   }
 
   function handleSubmit(event) {
