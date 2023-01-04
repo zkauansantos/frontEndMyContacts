58826729f23519f1c7469921572185f91352663e
zkauansantos <kauansantosdepontes@gmail.com>
2023-01-04 14:16:29 -0300

    starting implementation on edit page

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,15 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import Loader from '../../components/Loader';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
-    return (
-      <>
-        <PageHeader
-          title="Editar Mateus Silva "
-        />
-        <ContactForm buttonLabel="Cadastrar Contato" />
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const history = useHistory();
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({ type: 'danger', text: 'Ocorreu um erro ao obter o contato' });
+      }
+    }
 
-      </>
+    loadContact();
+  }, [id, history]);
+  function handleSubmit() {
+    //
+  }
 
-    );
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+      <PageHeader
+        title="Editar Mateus Silva "
+      />
+      <ContactForm
+        onSubmit={handleSubmit}
+        buttonLabel="Salvar Alterações"
+      />
+    </>
+
+  );
 }
--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -5,12 +5,16 @@ class ContactsService {
     this.httpClient = new HttpClient('http://localhost:3001');
   }
 
-  async listContacts(orderBy = 'asc') {
+  listContacts(orderBy = 'asc') {
     return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 
-  async createContact(contact) {
+  createContact(contact) {
     return this.httpClient.post('/contacts', { body: contact });
+  }
+
+  getContactById(id) {
+    return this.httpClient.get(`/contacts/${id}`);
   }
 }
 
